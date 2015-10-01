431e38dc21daddae5d25b67980b44000afd0f87b
David Hayes <drhayes@gmail.com>
2015-10-01 16:08:46 -0500

    Converting to metalsmith. Trying again!

---
+++ index.js
@@ -0,0 +1,53 @@
+var Metalsmith = require('metalsmith');
+var markdown = require('metalsmith-markdown');
+var templates = require('metalsmith-templates');
+var collections = require('metalsmith-collections');
+var permalinks = require('metalsmith-permalinks');
+var Handlebars = require('handlebars');
+var fs = require('fs');
+
+// Handlebars.registerPartial('header', fs.readFileSync(__dirname + '/templates/partials/header.hbt').toString());
+// Handlebars.registerPartial('footer', fs.readFileSync(__dirname + '/templates/partials/footer.hbt').toString());
+
+// var findTemplate = function(config) {
+//     var pattern = new RegExp(config.pattern);
+//
+//     return function(files, metalsmith, done) {
+//         for (var file in files) {
+//             if (pattern.test(file)) {
+//                 var _f = files[file];
+//                 if (!_f.template) {
+//                     _f.template = config.templateName;
+//                 }
+//             }
+//         }
+//         done();
+//     };
+// };
+
+Metalsmith(__dirname)
+  .use(collections({
+    pages: {
+      pattern: 'content/pages/*.md'
+    },
+    posts: {
+      pattern: 'content/posts/*.md',
+      sortBy: 'date',
+      reverse: true
+    }
+  }))
+  // .use(findTemplate({
+  //   pattern: 'posts',
+  //   templateName: 'post.hbt'
+  // }))
+  .use(markdown())
+  .use(permalinks({
+    pattern: ':collection/:title'
+  }))
+  .use(templates('handlebars'))
+  .destination('./build')
+  .build(function(err, files) {
+    if (err) {
+      throw err;
+    }
+  });
