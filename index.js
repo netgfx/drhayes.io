266d030863df853b3bcf4c48f7bbb332e6ab676c
David Hayes <drhayes@gmail.com>
2015-10-01 21:31:04 -0500

    Move breadcrumbs out to a lib.

--- index.js
+++ index.js
@@ -33,21 +33,7 @@ Metalsmith(__dirname)
       reverse: true
     }
   }))
-  // HACK: TOTALLY NOT GONNA WORK WITH ANY DEPTH!
-  .use(function (files, metalsmith, done) {
-    for (var filename in files) {
-      var file = files[filename];
-      if (!file.breadcrumbs) {
-        file.breadcrumbs = filename.split('/').slice(0, -1).map(function (segment) {
-          return {
-            link: segment,
-            title: morph.toTitle(segment.replace('.html', ''))
-          };
-        });
-      }
-    }
-    done();
-  })
+  .use(breadcrumbs())
   .use(markdown())
   .use(layouts({
     engine: 'handlebars',
