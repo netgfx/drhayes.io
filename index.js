5a9499d17a56d171ca1ff5d42958152bf7ee74a1
David Hayes <drhayes@gmail.com>
2015-10-08 12:26:46 -0500

    Refactor markdown parsing out of main file.

--- index.js
+++ index.js
@@ -1,81 +1,21 @@
 var Metalsmith = require('metalsmith');
 var watch = require('metalsmith-watch');
-var markdown = require('metalsmith-markdown');
 var collections = require('metalsmith-collections');
 var permalinks = require('metalsmith-permalinks');
 var layouts = require('metalsmith-layouts');
 var Handlebars = require('handlebars');
 var less = require('metalsmith-less');
 var moment = require('moment');
-var prism = require('prismjs');
-var marked = require('marked');
 var fs = require('fs');
 
 var breadcrumbs = require('./lib/breadcrumbs');
+var markdown = require('./lib/markdown');
 
 Handlebars.registerPartial('header', fs.readFileSync(__dirname + '/templates/partials/header.html', 'utf8'));
 Handlebars.registerPartial('footer', fs.readFileSync(__dirname + '/templates/partials/footer.html', 'utf8'));
 Handlebars.registerHelper('dateFormat', function (dateStr) {
   return moment(dateStr).format('MMMM D, YYYY');
 });
-
-// via https://glen.codes/using-prism-with-metalsmith-and-markdown/
-// Translate marked languages to prism.
-var extensions = {
-  js: 'javascript',
-  scss: 'css',
-  sass: 'css',
-  html: 'markup',
-  svg: 'markup',
-  xml: 'markup',
-  py: 'python',
-  rb: 'ruby',
-  ps1: 'powershell',
-  psm1: 'powershell'
-};
-
-var markdownOptions = {
-  highlight: function(code, lang) {
-    if (!prism.languages.hasOwnProperty(lang)) {
-      // Default to markup if it's not in our extensions.
-      lang = extensions[lang] || 'javascript';
-    }
-
-    return prism.highlight(code, prism.languages[lang]);
-  }
-};
-
-var renderer = new marked.Renderer();
-
-// Change the code method to output the same as Prism.js would.
-renderer.code = function(code, lang, escaped) {
-  code = this.options.highlight(code, lang);
-
-  if (!lang) {
-    return '<pre><code>' + code + '</code></pre>';
-  }
-
-  // e.g. "language-js"
-  var langClass = this.options.langPrefix + lang;
-
-  return '<pre class="' + langClass + '"><code class="' + langClass + '">' +
-    code +
-    '</code></pre>';
-};
-
-var markdownOptions = {
-  gfm: true,
-  smartypants: true,
-  renderer: renderer,
-  langPrefix: 'language-',
-  highlight: function(code, lang) {
-    if (!prism.languages.hasOwnProperty(lang)) {
-      // Default to markup.
-      lang = extensions[lang] || 'markup';
-    }
-    return prism.highlight(code, prism.languages[lang]);
-  }
-};
 
 Metalsmith(__dirname)
   .use(watch({
@@ -95,7 +35,7 @@ Metalsmith(__dirname)
     }
   }))
   .use(breadcrumbs())
-  .use(markdown(markdownOptions))
+  .use(markdown)
   .use(layouts({
     engine: 'handlebars',
     directory: 'templates',
